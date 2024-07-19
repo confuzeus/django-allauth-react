d8d4750bdc614578e4cceed9a501ab960b15411f
Josh Karamuth <hello@joshkaramuth.com>
2024-07-19 16:33:48 +0400

    Add Body

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-function App() {
+import { Container } from "@mui/material";
+import Body from "./components/Body";
+import useAuthSession from "./hooks/useAuthSession";
 
+function App() {
+  useAuthSession();
   return (
-    <div className="container my-20">
-      <h1 className="text-4xl text-center">React + Allauth</h1>
-    </div>
-  )
+    <Container component="main" sx={{ margin: "10vh auto" }}>
+      <h1 style={{ textAlign: "center" }}>React + Allauth</h1>
+      <Body />
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Body/index.tsx
@@ -0,0 +1,38 @@
+import { useAtomValue } from "jotai";
+import LoginForm from "../LoginForm";
+import { genericStore } from "../../store/generic";
+import { CurrentAction } from "../../store/generic/types";
+import { Container } from "@mui/material";
+import { sessionStore } from "../../store/sessions";
+import LogoutButton from "../LogoutButton";
+import CrudBody from "../CrudBody";
+
+export default function Body() {
+  const isAuthenticated = useAtomValue(sessionStore.isAuthenticatedAtom);
+  const currentAction = useAtomValue(genericStore.currentActionAtom);
+
+  let body;
+
+  if (currentAction === CurrentAction.Signin) {
+    body = <LoginForm />;
+  } else if (currentAction === CurrentAction.Authenticated) {
+    body = <CrudBody />;
+  }
+
+  return (
+    <Container component="div">
+      {isAuthenticated && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "3rem",
+          }}
+        >
+          <LogoutButton />
+        </div>
+      )}
+      {body}
+    </Container>
+  );
+}
