59325337b6a6221b6418dfb0af8880ac8bc90357
Josh Karamuth <hello@joshkaramuth.com>
2024-07-19 15:17:18 +0400

    Add QueryClieentProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,10 +6,15 @@ import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
