11adab8ef210cee1477becf024d2948becf95a45
Josh Karamuth <hello@joshkaramuth.com>
2024-07-20 11:18:29 +0400

    Set loading state on logout

--- frontend/src/components/LogoutButton.tsx
+++ frontend/src/components/LogoutButton.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@mui/material";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logoutMutation } from "../api/accounts/logout";
+import { useSetAtom } from "jotai";
+import { genericStore } from "../store/generic";
 
 export default function LogoutButton() {
   const queryClient = useQueryClient();
+  const setIsLoading = useSetAtom(genericStore.isLoadingAtom);
   const { mutate } = useMutation({
     mutationFn: logoutMutation,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["authSession"] });
+      setIsLoading(true);
     },
   });
   return (
