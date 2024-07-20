1c7a30acf4ec1b6802173e0ac4ccc875dedf1188
Josh Karamuth <hello@joshkaramuth.com>
2024-07-20 20:10:12 +0400

    Add api for getting server auth config

--- frontend/src/api/auth/index.ts
+++ frontend/src/api/auth/index.ts
@@ -1,4 +1,25 @@
-export async function getConfigurationQuery() {
+interface SocialAccountProvider {
+  id: string;
+  name: string;
+  flows: Array<"provider_redirect" | "provider_token">;
+  client_id: string;
+}
+interface SocialAccountData {
+  providers: SocialAccountProvider[];
+}
+interface AccountData {
+  authentication_method: string;
+  is_open_for_signup: boolean;
+}
+interface ConfigurationData {
+  account: AccountData;
+  socialaccount: SocialAccountData;
+}
+interface ConfigurationResponse {
+  status: 200;
+  data: ConfigurationData;
+}
+async function getConfigurationQuery(): Promise<ConfigurationResponse> {
   const response = await fetch(
     `${import.meta.env.VITE_API_URL}/_allauth/browser/v1/config`,
     { credentials: "include" },
@@ -9,3 +30,7 @@ export async function getConfigurationQuery() {
   }
   return responseData;
 }
+
+export const authApi = {
+  getConfigurationQuery,
+};
